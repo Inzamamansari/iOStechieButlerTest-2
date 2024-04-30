75b1d3fc6c80a93df85a3bd39d6152c830d88b53
Inzimamul Ansari <inzimamul.ansari@infosys.com>
2024-04-30 20:56:14 +0530

    Initial Commit

---
+++ iOStechieButlerTest/iOStechieButlerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOStechieButlerTestApp.swift
+//  iOStechieButlerTest
+//
+//  Created by Inzimamul Ansari on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iOStechieButlerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
